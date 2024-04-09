670c32caa9fdd182e3d9a4c537f6206fb997c8f8
valerioviale <34212301+valerioviale@users.noreply.github.com>
2024-04-09 10:22:19 +0200

    Update main.py
    Monthly connections

--- main.py
+++ main.py
@@ -6,25 +6,27 @@ lkd = pd.read_csv('Linkedin_Connections_10k_2_Feb_2024.csv')
 
 # Convert 'Connected On' column to datetime format
 lkd['Connected On'] = pd.to_datetime(lkd['Connected On'], format="%d/%m/%Y")
-
 # Display the first 20 rows and the shape of the DataFrame
 print(lkd.head(20))
 print(lkd.shape)
 
-# Sort the DataFrame by 'Connected On' column
-lkd = lkd.sort_values(by='Connected On')
+# Group by month and year, and convert Period to string
+monthly_connections = lkd.groupby(lkd['Connected On'].dt.to_period("M")).size().reset_index(name='Number of Connections')
+monthly_connections['Connected On'] = monthly_connections['Connected On'].astype(str)
 
 # Create a line plot using Plotly Express
 fig = px.line(
-    lkd.groupby(by='Connected On').count().reset_index(),
+    monthly_connections,
     x='Connected On',
-    y='First Name',
-    labels={'First Name': 'Number of Connections'},
-    title='Connection Timeline')
+    y='Number of Connections',
+    labels={'Number of Connections': 'Number of Connections'},
+    title='Monthly Connection Timeline'
+)
 
-# Save the plot as an HTML file
-fig.write_html('chart.html')
 
-# Open the HTML file in the default web browser
-import webbrowser
-webbrowser.open('chart.html')
+
+# Show the plot
+fig.show()
+
+
+
